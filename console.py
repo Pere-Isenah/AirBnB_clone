5bcb1ef15b07c3b7b1fc979fd6f00854a592b694
Pere-Isenah <pereisenah@gmail.com>
2023-03-09 10:44:48 +0100

    fix: moved base_model.py to the right dir

--- console.py
+++ console.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
+import json
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
@@ -14,6 +17,7 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = "(hbnb)"
+    __classes = {"BaseModel"}
 
     def do_quit(self, arg):
         """
@@ -50,6 +54,17 @@ class HBNBCommand(cmd.Cmd):
         Display help text for the EOF command.
         """
         print("EOF command to exit the program")
+
+    def emptyline(self):
+        """Handles empty lines"""
+        pass
+ 
+    def do_create(self, arg):
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
