f8ac51b04891410eb3c6bc1102aa3247bb882733
Reich-imperial <olanipekunayodele01@gmail.com>
2023-03-09 04:02:04 -0800

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,57 +1,54 @@
 #!/usr/bin/python3
-"""
-This module defines the BaseModel class, which provides common attributes
-and methods for all models.
-"""
-
 from datetime import datetime
 import uuid
 
+""" This module defines a class `BaseModel` """
+
 
 class BaseModel():
-    """
-    The base model class provides common attributes and methods for all models.
+    """ defines all common attributes/methods for other classes """
 
-    Attributes:
-        id (str): A unique identifier for the model instance.
-        create_at (datetime): The datetime when the model instance was created.
-        updated_at (datetime): The datetime when the model instance
-        was last updated.
-    """
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of BaseModel
 
-    def __init__(self):
+        Args:
+            *args (any): unused
+            **kwargs (dict): key & value pair attributes
         """
-        Constructor method that initializes the id, create_at,
-        and updated_at attributes.
-        """
-        self.id = str(uuid.uuid4())
-        self.create_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue;
+                elif key == "updated_at" or key == "created_at":
+                    self.__dict__[key] = datetime.fromisoformat(value);
+                else:
+                    self.__dict__[key] = value;
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-        Returns a string representation of the object with the class name,
-        id, and attributes.
-        """
+        """ Defines the string representation of the instance """
+
         return f"[{self.__class__.__name__}],({self.id}),{self.__dict__}"
 
     def save(self):
-        """
-        Updates the updated_at attribute with the current datetime.
-        """
+        """ updates the public instance attribute `updated_at` """
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """ returns a dictionary containing all
+            key/value of `__dict__` of the instance
         """
-        Returns a dictionary representation of the object with the class name,
-        create_at, and updated_at attributes.
 
-        Returns:
-            dict: A dictionary representation of the object.
-        """
         dict_copy = self.__dict__.copy()
-        dict_copy.update({"__class__": self.__class__.__name__})
-        dict_copy.update({"create_at": self.create_at.isoformat()})
-        dict_copy.update({"updated_at": self.updated_at.isoformat()})
-
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
         return dict_copy
+
+
+    
