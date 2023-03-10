a562e6204e24703955e039186880e916e3570289
Reich-imperial <olanipekunayodele01@gmail.com>
2023-03-10 13:51:31 -0800

    task 5 done, code structre still messy

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 from datetime import datetime
 import uuid
 import models
-
 """ This module defines a class `BaseModel` """
 
 
@@ -29,6 +28,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """ Defines the string representation of the instance """
@@ -39,6 +39,7 @@ class BaseModel():
         """ updates the public instance attribute `updated_at` """
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all
